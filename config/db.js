68f78183f73a3ff11174ea214dd0e0e254b19fa9
meitian <meitian3611@163.com>
2019-11-20 17:31:56 +0800

    config

--- config/db.js
+++ config/db.js
@@ -1,1 +1,26 @@
-console.log(222);
+// 链接mongodb数据库的文件
+
+
+// 1. 引入 mongoose
+const mongoose = require('mongoose')
+
+// 2. 定义链接的地址
+const url = 'mongodb://127.0.0.1:27017/express'
+
+// 3. 通过 mongoose.connect()去链接，方法返回的是一个 promise 对象
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        //链接成功
+        console.log('数据库链接成功');
+    })
+    .catch(error => {
+        //链接失败
+        console.log('数据库链接失败');
+        console.log(error);
+    })
+
+// 4. 将已经链接到 mongodb 的 mongoose 模块给暴露出去
+module.exports = mongoose
--- models/post.js
+++ models/post.js
@@ -1,1 +1,26 @@
-console.log(222);
+// post表相关的model文件
+
+// 1. 引入之前已经链接到mongdb数据库的mongoose模块
+const mongoose = require('../config/db')
+
+// 2. 实例化一个 schema(描述表的结构的东西)
+const schema = new mongoose.Schema({
+    //键值对，key：表的字段名 value：这个字段的类型
+
+    title: {//文章标题
+        type: String,
+        required: true
+    },
+    body: {//文章正文
+        type: String,
+        required: true
+    }
+})
+
+
+// 3. 通过 mongoose.model()生成当前post 的model
+// 第一个参数，是我们的表名的单数形式
+const model = mongoose.model('post', schema)
+
+// 4. 暴露出去
+module.exports = model
